18ec15bd5d2e2d23a7d5641630aeff943567fa35
Stuart Swindells <github@lumo.me.uk>
2013-04-20 14:12:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spaceapps::Application.config.secret_token = '0f8af4cf711f9257aa697b1ba1e0d2982249e63d4d959a47ff16d2667c83e17ce4252f32a3c1f80c5f5db4e058bc8b801a8133d30d130d499c842bd5a0890189'
