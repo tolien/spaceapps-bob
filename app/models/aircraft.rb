04c55fa8858b5821946e03fbada6f7a811f3d2c0
Stuart Swindells <github@lumo.me.uk>
2013-04-20 16:21:17 +0100

    Working associations...

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,3 +1,6 @@
 class Aircraft < ActiveRecord::Base
   attr_accessible :code
+  
+  belongs_to :destination, class_name: "Airport", foreign_key: "destination_id"
+  belongs_to :source, class_name: "Airport", foreign_key: "source_id"
 end
