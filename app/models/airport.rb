d263c1b4eef2fac7e9f026beb8f3dd5a24a5fe6e
Stuart Swindells <github@lumo.me.uk>
2013-04-20 16:51:40 +0100

    Make latitude/longitude accessibility, validate for code/name and numericality of lat/long

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,12 @@
 class Airport < ActiveRecord::Base
-  attr_accessible :code, :name
+  attr_accessible :code, :name, :latitude, :longitude
   
   has_many :arriving_aircraft, class_name: 'Aircraft', foreign_key: 'destination_id'
   has_many :departing_aircraft, class_name: 'Aircraft', foreign_key: 'source_id'
+  
+  validates_presence_of :code, :name
+  
+  validates_numericality_of :latitude
+  validates_numericality_of :longitude
+  
 end
