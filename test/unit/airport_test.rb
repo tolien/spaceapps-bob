e33e402a78e4c6ea58a7ba1b779ac56cafe08dde
Stuart Swindells <github@lumo.me.uk>
2013-04-20 17:33:27 +0100

    Our first unit test...

--- test/unit/airport_test.rb
+++ test/unit/airport_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AirportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @yyz = airports(:yyz)
+  end
+  
+  test "airport coordinate validity" do
+    airport = @yyz
+    airport.latitude = nil
+    airport.longitude = nil
+    
+    assert airport.invalid?
+  end
 end
