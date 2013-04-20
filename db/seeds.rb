349b621247888f2d7009a63fb3367ebf615d7ee4
Stuart Swindells <github@lumo.me.uk>
2013-04-20 17:26:49 +0100

    Add YYZ and JFK to the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@
 
 Airport.create(code: 'GLA', name: 'Glasgow International', latitude: 38.898556, longitude: -77.037852)
 Airport.create(code: 'LHR', name: 'London Heathrow', latitude: 51.4775, longitude: -0.461389)
+Airport.create(name: 'Toronto Pearson International', code: 'YYZ', latitude: 43.676667, longitude: -79.630556)
+Airport.create(name: 'John F. Kennedy International', code: 'JFK', latitude: 40.639722, longitude: -73.778889)
