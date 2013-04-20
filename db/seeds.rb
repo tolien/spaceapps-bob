8cc86030fe35c9b6b1d4c9c0c031c59c40c81145
Stuart Swindells <github@lumo.me.uk>
2013-04-20 16:41:15 +0100

    Add GLA to the list of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Airport.create(code: 'GLA', name: 'Glasgow International', latitude: 38.898556, longitude: -77.037852)
