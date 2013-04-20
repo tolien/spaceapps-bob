9640da3e7608efb52b56db553c6043e32534d52f
Stuart Swindells <github@lumo.me.uk>
2013-04-20 16:52:21 +0100

    Add lat/long to the schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130420140640) do
+ActiveRecord::Schema.define(:version => 20130420152625) do
 
   create_table "aircrafts", :force => true do |t|
     t.string   "code"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(:version => 20130420140640) do
     t.string   "code"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
