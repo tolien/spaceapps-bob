8ce17d864848f87402de2dde6d289c797a45f7ff
Stuart Swindells <github@lumo.me.uk>
2013-04-20 16:50:32 +0100

    Make destination/source airport IDs, validate for presence of aircraft code, destination/source airports

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,6 +1,8 @@
 class Aircraft < ActiveRecord::Base
-  attr_accessible :code
+  attr_accessible :code, :destination_id, :source_id
   
   belongs_to :destination, class_name: "Airport", foreign_key: "destination_id"
   belongs_to :source, class_name: "Airport", foreign_key: "source_id"
+  
+  validates_presence_of :code, :destination, :source
 end
