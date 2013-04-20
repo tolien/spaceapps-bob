5369df92101b57dd37c642124ad26dfb6e02cf46
Andrew Smillie <ajsmillie@gmail.com>
2013-04-20 19:28:42 +0100

    Canvas Visualisation (start of...)

---
+++ app/controllers/visualisation_controller.rb
@@ -0,0 +1,11 @@
+class VisualisationController < ApplicationController
+  
+  # GET /visualisation
+  def index
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+  
+end
