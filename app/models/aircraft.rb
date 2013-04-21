33e377a42a75cb5a303834928d3e77125feda770
Stuart Swindells <github@lumo.me.uk>
2013-04-21 13:16:56 +0100

    More validations for aircraft model

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -4,7 +4,10 @@ class Aircraft < ActiveRecord::Base
   belongs_to :destination, class_name: "Airport", foreign_key: "destination_id"
   belongs_to :source, class_name: "Airport", foreign_key: "source_id"
   
-  validates_presence_of :code, :destination, :source
+  validates_presence_of :code, :destination, :source, :latitude, :longitude
+  
+  validates_numericality_of :latitude
+  validates_numericality_of :longitude
   
   extend FriendlyId
   friendly_id :code, use: :slugged
