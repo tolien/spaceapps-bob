f9b993c08d8e6df5e9d1d05972ac8b3f333d397a
Stuart Swindells <github@lumo.me.uk>
2013-04-21 13:11:20 +0100

    Remove the default lat/long values from aircraft

--- db/migrate/20130421033847_add_lat_long_to_aircraft.rb
+++ db/migrate/20130421033847_add_lat_long_to_aircraft.rb
@@ -1,7 +1,7 @@
 class AddLatLongToAircraft < ActiveRecord::Migration
   def change
-    add_column :aircrafts, :latitude, :float, :default => 0
-    add_column :aircrafts, :longitude, :float, :default => 0
+    add_column :aircrafts, :latitude, :float
+    add_column :aircrafts, :longitude, :float
   end
 end
 
