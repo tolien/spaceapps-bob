70f71aad4357a98006a3c1da664826b48bdce3cd
Stuart Swindells <github@lumo.me.uk>
2013-04-21 13:07:56 +0100

    Add some aircraft model validation

--- test/unit/aircraft_test.rb
+++ test/unit/aircraft_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class AircraftTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "aircraft validation" do
+    aircraft = Aircraft.create
+    
+    assert aircraft.invalid?
+    assert aircraft.errors[:code].include?("can't be blank")
+    assert aircraft.errors[:source].include?("can't be blank")
+    assert aircraft.errors[:destination].include?("can't be blank")
+    assert aircraft.errors[:latitude].include?("can't be blank")
+    assert aircraft.errors[:longitude].include?("can't be blank")
+    
+    aircraft.latitude = 'over'
+    aircraft.longitude = 'there'
+    assert aircraft.invalid?
+    
+    assert aircraft.errors[:latitude].include?("is not a number")
+    
+  end
 end
