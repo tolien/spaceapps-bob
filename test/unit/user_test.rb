f80c34d072ed5910ed9580d4a72ad5a122099771
Stuart Swindells <github@lumo.me.uk>
2013-04-22 23:54:36 +0100

    Add to the user model tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  
+  setup do
+    @user_one = users(:one)
+  end
+  
   test "user validation" do
     user = User.create
     assert user.invalid?
@@ -11,6 +16,34 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "username uniqueness" do
+    user = User.create
+    user.username = @user_one.username
+    user.email = "2" + @user_one.email
+    user.password = 'password'
+    user.password_confirmation = 'password'
+    
+    user.id = nil
+    assert user.invalid?
+    assert user.errors[:username].include?("has already been taken")
+  end
   
+  test "password confirmation" do
+    user = User.create
+    user.username = @user_one.username + "2"
+    user.email = "2" + @user_one.email
+    user.password = 'password'
+    user.password_confirmation = 'password'
+    
+    assert user.valid?
+    
+    user.password = 'password1'
+    assert user.invalid?
+    assert user.errors[:password].include?("doesn't match confirmation")
+    
+    user.password_confirmation = 'password1'
+    assert user.valid?
+    assert_nothing_raised do
+      user.save!
+    end
   end
 end
