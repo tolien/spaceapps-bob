622c594d60962810969c64927e0181384823ce5c
Stuart Swindells <github@lumo.me.uk>
2013-05-11 21:14:37 +0100

    whitespace removal

--- app/controllers/visualisation_controller.rb
+++ app/controllers/visualisation_controller.rb
@@ -2,7 +2,6 @@ class VisualisationController < ApplicationController
   # before_filter :authenticate_user!
   # GET /visualisation
   def index
-
     respond_to do |format|
       format.html # index.html.erb
     end
